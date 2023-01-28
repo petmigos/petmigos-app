5274ca21119379cbea2a79cbb8d5d33765a2a9cf
artuguen28 <87493045+artuguen28@users.noreply.github.com>
2023-01-28 15:39:37 -0400

    Cadastro-adotante frontend almost finished
    To do: organize the components, load the Ubuntu fonts and start the backend.

--- App.tsx
+++ App.tsx
@@ -1,42 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, TouchableOpacity, StyleSheet, Text, TextInput, View, Button, SafeAreaView } from 'react-native';
+import Logo from "./assets/logo.svg";
+import { Linking } from 'react-native';
+import DropShadow from "react-native-drop-shadow";
 
 export default function App() {
-
 
 	return (
 		<View style={styles.container}>
 
-			<Image style={styles.logo_img} source={require('./assets/petmigos_logo_login.png')} />
+			<Logo style={styles.logo_img}/>
 			<Text style={styles.titleText}>
 				Cadastro
 			</Text>
-			<TextInput style={styles.input_username}
-				placeholder="Email">
-			</TextInput>
-			<TextInput style={styles.input_username}
-				placeholder="Nome de Usuário">
-			</TextInput>
-			<TextInput style={styles.input_username}
-				placeholder="Digite sua senha">
-			</TextInput>
-			<TextInput style={styles.input_username}
-				placeholder="Digite sua senha">
-			</TextInput>
-			<TouchableOpacity style={styles.acessing_button}>
-				<Text style={styles.getin_text}>
-					CADASTRAR
+			<View style={styles.middle_screen}>
+				<TextInput style={styles.input_box}
+					placeholder="Email">
+				</TextInput>
+				<TextInput style={styles.input_box}
+					placeholder="Nome de Usuário">
+				</TextInput>
+				<TextInput style={styles.input_box}
+					placeholder="Digite sua senha">
+				</TextInput>
+				<TextInput style={styles.input_box}
+					placeholder="Digite sua senha">
+				</TextInput>
+				<TouchableOpacity style={styles.acessing_button}>
+					<Text style={styles.getin_text}>
+						CADASTRAR
+					</Text>
+				</TouchableOpacity>
+				<Text style={styles.privacy_text}>
+					Ao se cadastrar você concorda com nossos 
+					<Text style={styles.privacy_text_link} onPress={() => Linking.openURL('https://google.com')}> termos de uso </Text>
+					e 
+					<Text style={styles.privacy_text_link} onPress={() => Linking.openURL('https://youtube.com')}> política de privacidade</Text>
 				</Text>
-			</TouchableOpacity>
+			</View>
 
-			<Text style={styles.privacy_text}>
-				Não tem uma conta?
-			</Text>
-			<TouchableOpacity style={styles.sign_up_button}>
-				<Text style={styles.sign_up_text}>
-					Cadastre-se!
-				</Text>
-			</TouchableOpacity>
+
 
 			<StatusBar style="auto" />
 		</View>
@@ -53,43 +56,44 @@ const styles = StyleSheet.create({
 	},
 
 	logo_img: {
-		top: -100,
+		marginBottom: 10,
 	},
 
 	titleText: {
 		color: '#000000',
-		fontFamily: 'Ubuntu',
+		// fontFamily: 'Ubuntu-Bold',
 		fontWeight: "bold",
-		top: -110,
 		fontSize: 48,
 
 	},
-	input_username: {
+
+	middle_screen: {
+		backgroundColor: '#FFFFFF',
+		width: 300,
+	},
+
+	input_box: {
 		height: 57,
-		width: 343,
-		margin: 12,
+		marginTop: 20,
 		borderWidth: 1,
 		padding: 10,
-		top: -100,
 		backgroundColor: '#DCDCDC',
 		borderRadius: 6,
 		opacity: 0.5,
 		fontSize: 18,
 		borderColor: '#fff',
 	},
-
 	acessing_button: {
 		backgroundColor: '#915E36',
-		width: 343,
 		height: 56,
-		fontFamily: 'Ubuntu_400Regular',
+		// fontFamily: 'Ubuntu-Bold',
 		fontStyle: 'normal',
 		alignItems: 'center',
 		textAlign: 'center',
-		marginTop: 20,
-		top: -80,
+		marginTop: 30,
 		borderRadius: 8,
 	},
+
 
 	getin_text: {
 		fontSize: 18,
@@ -98,39 +102,20 @@ const styles = StyleSheet.create({
 		color: '#FFFFFF'
 	},
 
-	forgot_pass_text: {
-		color: '#000000',
-		fontFamily: 'Ubuntu_400Regular',
-		fontStyle: 'normal',
-		top: -66,
-		fontSize: 14,
-		opacity: 0.5,
+	privacy_text_link: {
+		color: '#A04D0B',
+		textDecorationLine: "underline",
 	},
 
 	privacy_text:
 	{
 		color: '#000000',
-		fontFamily: 'Ubuntu_400Regular',
+		// fontFamily: 'Ubuntu-Regular',
 		fontStyle: 'normal',
-		top: 20,
-		fontSize: 14,
-		left: -45,
+		paddingTop: 10,
+		textAlign: 'center',
+		fontSize: 12,
 	},
-	sign_up_button:
-	{
-		border: 'none',
-		style: 'transparent',
-		color: 'white',
-		top: 1,
-		left: 70,
-	},
-	sign_up_text:
-	{
-		color: 'black',
-		fontWeight: 'bold',
-		textDecorationLine: 'underline',
-		fontSize: 14,
-	}
 
 
 });
