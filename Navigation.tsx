f8320d60cdd5c7928e99f2d260106037145aba58
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2023-03-01 15:49:22 -0300

    :sparkles: add store navigation

--- Navigation.tsx
+++ Navigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -10,20 +11,30 @@ import PickUpSignUp from './src/screens/PickUpSignUp';
 import SignUpCompany from './src/screens/SignUpCompany';
 import CadastroScreen from './src/screens/CadastroScreen';
 
+//import PetStoreScreen from './src/screens/PetStoreScreen';
+//import StoreList from './src/screens/Stores/StoreList';
+//import StorePage from './src/screens/Stores/StorePage';
+
 import ListPets from './src/screens/Pets';
 import PetDetails from './src/screens/PetDetails';
 
+//import Padrinhos from './src/screens/Padrinhos/Padrinhos';
 import Profile from './src/screens/Profile';
+
+// --- TELAS DE EMPRESA
+//import MyStoreScreen from './src/screens/MyStoreScreen';
+//import OrdersScreen from './src/screens/OrdersScreen';
+//import StoreProfileScreen from './src/screens/StoreProfileScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function TabNavigator() {
+function TabPetOwner() {
   return (
     <Tab.Navigator screenOptions={{
         headerShown: false,
         tabBarActiveTintColor: primary,
-        tabBarStyle: {position: 'absolute', height: '6%', paddingBottom: 5}}}
+        tabBarStyle: style.tab}}
     >
         <Tab.Screen name="PetStoreStack" component={PetStoreStack} 
         options={{
@@ -49,6 +60,37 @@ function TabNavigator() {
           }}/>
     </Tab.Navigator>
   );
+}
+
+function TabCompany(){
+
+    return(
+      <Tab.Navigator screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: primary,
+        tabBarStyle: style.tab}}
+      >
+        {/*<Tab.Screen name="MyStoreScreen" component={MyStoreScreen}
+          options={{
+            tabBarLabel: 'Minha Loja',
+          }}/>
+        <Tab.Screen name="OrdersScreen" component={OrdersScreen}
+          options={{
+            tabBarLabel: 'Pedidos',
+          }}
+        />
+        <Tab.Screen name="StoreProfileScreen" component={StoreProfileScreen}
+          options={{
+            tabBarLabel: 'Meu perfil',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person-circle-outline" size={size} color={color} />
+            )
+          }}
+        />*/}
+
+      </Tab.Navigator>
+    )
+
 }
 
 function AuthStack() {
@@ -78,32 +120,38 @@ function PetStoreStack(){
   return(
     <Stack.Navigator
       screenOptions={{headerShown: false}} initialRouteName="PetStore">
-        <Stack.Screen name="Pets" component={ListPets} />
-        {/*<Stack.Screen name="PetStore" component={PetStoreScreen}/>
+        {/* {<Stack.Screen name="PetStore" component={PetStoreScreen}/>
         <Stack.Screen name="StoreList" component={StoreList}/>
-        <Stack.Screen name="StorePage" component={StorePage}/>*/}
+        <Stack.Screen name="StorePage" component={StorePage}/>} */}
     </Stack.Navigator>
   )
 }
 
-/*function StoreStack(){
+function StoreStack(){
     return(
         <Stack.Navigator
         screenOptions={{headerShown: false}} initialRouteName="StoreList">
-            <Stack.Screen name="StoreList" component={StoreList}/>
+            {/* {<Stack.Screen name="StoreList" component={StoreList}/>} */}
         </Stack.Navigator>
     )
-}*/
+}
 
 export default function NavigateTo() {
     return (
         <NavigationContainer>
           <Stack.Navigator>
           {/*<Stack.Screen name="Auth" component={AuthStack} options={{ headerShown: false }} />*/}
-          <Stack.Screen name="TabNavigator" component={TabNavigator} options={{ headerShown: false }} />
+          <Stack.Screen name="TabPetOwner" component={TabPetOwner} options={{ headerShown: false }} />
+          <Stack.Screen name="TabCompany" component={TabCompany} options={{ headerShown: false }} />
           <Stack.Screen name="PetStack" component={PetStack} options={{ headerShown: false }} />
           <Stack.Screen name="PetStoreStack" component={PetStoreStack} options={{headerShown: false}}/>  
         </Stack.Navigator>
         </NavigationContainer>
       );
 }
+
+const style = StyleSheet.create({
+  tab:{
+    position: 'absolute', height: '6%', paddingBottom: 5
+  }
+})
